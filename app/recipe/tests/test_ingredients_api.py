aba43388d674223ca68ce2a2e0f12b0a31dd1460
Hoffmann Jens <HOFFMJ@de.softlab.net>
2023-05-26 20:02:33 +0200

    Fixed migrations

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -12,3 +12,22 @@ from core.models import Ingredient
 
 from recipe.serializers import IngredientSerializer
 
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return user."""
+    return get_user_model().objects.create_user(email=email, password=password)
+
+class PublicIngredientsApiTests(TestCase):
+    """Test unauthenticated API requests."""
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required for retrieving ingredients."""
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
